6435da74c2b55ebdd762f4d68211666643d63ab6
dat <me@tatd.at>
2018-08-31 15:22:35 +0700

    general: Update export commands

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 cp -r profiles/        ~/
 cp    emacs/.spacemacs ~/
-cp -r karabiner/       ~/.config/karabiner/
+cp -r karabiner/       ~/.config/
 cp -r ssh              ~/.ssh
 gpg --import gpg/gpg.key
