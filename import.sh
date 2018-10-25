4dfd28499423216c4bddf822e0376a82c065ec40
dat <me@tatd.at>
2018-10-25 17:57:52 +0700

    Update import.sh scripts

--- import.sh
+++ import.sh
@@ -1,13 +1,20 @@
 mkdir -p profiles
-cp $HOME/.zshrc      profiles
-cp -r $HOME/.scripts profiles
-cp -r $HOME/.keys    profiles
-cp -r $HOME/.ssh     ssh
+cp $HOME/.zshrc            profiles
+cp -r $HOME/.scripts       profiles
+cp -r $HOME/.keys          profiles
+cp $HOME/.gitconfig        profiles
+cp $HOME/.gitignore_global profiles
+
+mkdir -p ssh
+cp -a $HOME/.ssh/. ssh
 
 mkdir -p emacs
 cp $HOME/.spacemacs ./emacs/
 
 mkdir -p karabiner
 cp -r $HOME/.config/karabiner ./karabiner
+
+mkdir -p whitefox
+cp HOME/whitefox/*.json whitefox
 
 gpg --export-secret-key -a $GPG_UID > gpg/gpg.key
